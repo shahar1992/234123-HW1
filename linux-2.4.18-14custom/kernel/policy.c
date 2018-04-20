a8d9aab199209481d8b5b778e83970f62218e046
shahar1992 <shahar92@campus.technion.ac.il>
2018-04-20 17:14:23 +0300

    20/04 - 17:14

--- linux-2.4.18-14custom/kernel/policy.c
+++ linux-2.4.18-14custom/kernel/policy.c
@@ -24,14 +24,82 @@ int sys_enable_policy (pid_t pid ,int size, int password){
 
     task->p_state = ALLOW_POLICY;
     task->p_lvl = LEVEL_2;
+
     return 0;
+}
 
 
+int sys_disable_policy (pid_t pid ,int password){
+    if(pid < 0)
+        return -ESRCH;
+
+    task_t* task = find_task_by_pid(pid);
+    if(!task)
+        return -ESRCH;
+    
+    if(task->p_state == BLOCK_POLICY)
+        return -EINVAL;
+
+    if(password != ADMIN_PSWD)
+        return -EINVAL;
+
+    if(size < 0)
+        return -EINVAL;
+
+    //delete the process log and free allocated memory
+
+    task->p_state = BLOCK_POLICY;
+
+    return 0;
+}
+
+
+int sys_set_process_capabilities(pid_t pid,int new_level,int password){
+    if(pid < 0)
+        return -ESRCH;
+
+    task_t* task = find_task_by_pid(pid);
+    if(!task)
+        return -ESRCH;
+    
+    if(new_level < 0 || new_level > 2)
+        return -EINVAL;
+
+    if(password != ADMIN_PSWD)
+        return -EINVAL;
+    
+    if(task->p_state == BLOCK_POLICY)
+        return -EINVAL;
+    
+    task->p_lvl = new_level;
+
+    return 0;
+}
+
+int get_process_log(pid_t pid,int size,struct forbidden_activity_info* user_mem)    
+    if(pid < 0)
+        return -ESRCH;
+
+    task_t* task = find_task_by_pid(pid);
+    if(!task)
+        return -ESRCH; 
+    
+    if(size > task->log.size)
+        return -EINVAL;
+
+    if(size < 0)
+        return -EINVAL;
+
+    if(task->p_state == BLOCK_POLICY)
+        return -EINVAL;
+    
+    user_mem = task->log;
+    /*change task.log to the size-th data*/
+
+    return 0;
 }
 
 
 
-// .long SYMBOL_NAME(sys_enable_policy) /* 243 */
-// 	.long SYMBOL_NAME(sys_disable_policy) /* 244 */
-// 	.long SYMBOL_NAME(sys_set_process_capabilities) /* 245 */
-// 	.long SYMBOL_NAME(sys_get_process_log)
+
+
